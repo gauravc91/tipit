f2cd05da8743678118d0c166edc8784ce95525a5
DECR7PT <decr7pt@gmail.com>
2016-08-09 09:27:10 +0000

    Add first_name column, users/edit and signup fixed acordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  VALID_NAME_REGEX = /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
+  validates(:first_name,  presence: true, length: { maximum: 13, minimum: 2 }, format: { with: VALID_NAME_REGEX })
+  before_save {self.first_name =  first_name.capitalize}
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, :omniauth_providers => [:facebook]
