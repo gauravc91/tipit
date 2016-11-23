eb5257e625a59d51947e3f4e755b69697328e12b
DECR7PT <decr7pt@gmail.com>
2016-11-23 02:03:54 +0000

    Disabled header nav when on show campaign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def disable_nav
+    if @campaign.user != current_user
+      @disable_nav = true
+    else
+      @disable_nav = false
+    end
+  end
+  
 end
--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,7 @@
 class CampaignsController < ApplicationController
     before_action :find_post, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, except: [:index, :show]
+    before_filter :disable_nav, only: [:show]
     
     def index
         @campaigns = Campaign.all.order("created_at DESC")
